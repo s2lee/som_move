995088ddaa1d6e8fc5d1ad251f8e7f6c3b46ff35
s2lee <therok.manager@gmail.com>
2023-03-23 21:16:27 +0900

    fix wait

--- main.py
+++ main.py
@@ -23,18 +23,27 @@ pyautogui.write(PASSWORD)
 pyautogui.press("enter")
 time.sleep(5)
 driver.switch_to.window(driver.window_handles[-1])
-
+time.sleep(5)
 array = []
-for i in range(15, 27):
+for i in range(27):
     project_div_idx1 = f"div[{2 if i else 1}]"
     project_div_idx2 = f"div[{i}]/div" if i else ""
-    driver.find_element(
-        By.XPATH, f'//*[@id="snb_middle"]/{project_div_idx1}/{project_div_idx2}/div/div'
-    ).click()
-    # time.sleep(5)
-    fold_count = driver.find_element(
-        By.XPATH,
-        f'//*[@id="snb_middle"]/{project_div_idx1}/{project_div_idx2}/div/span[2]',
+    project = WebDriverWait(driver, 2).until(
+        EC.element_to_be_clickable(
+            (
+                By.XPATH,
+                f'//*[@id="snb_middle"]/{project_div_idx1}/{project_div_idx2}/div/div',
+            )
+        )
+    )
+    project.click()
+    fold_count = WebDriverWait(driver, 2).until(
+        EC.visibility_of_element_located(
+            (
+                By.XPATH,
+                f'//*[@id="snb_middle"]/{project_div_idx1}/{project_div_idx2}/div/span[2]',
+            )
+        )
     )
     project_name = driver.find_element(
         By.XPATH,
@@ -42,18 +51,22 @@ for i in range(15, 27):
     )
     for j in range(int(fold_count.text)):
         item_selected_idx = f"div[{i}]" if i else ""
-        driver.find_element(
-            By.XPATH,
-            f'//*[@id="snb_middle"]/{project_div_idx1}/{item_selected_idx}/div/ul/li[{j + 1}]',
-        ).click()
-        # time.sleep(2)
-        date = WebDriverWait(driver, 4).until(
-            EC.presence_of_element_located(
+        memo_li = WebDriverWait(driver, 2).until(
+            EC.element_to_be_clickable(
+                (
+                    By.XPATH,
+                    f'//*[@id="snb_middle"]/{project_div_idx1}/{item_selected_idx}/div/ul/li[{j + 1}]',
+                )
+            )
+        )
+        memo_li.click()
+        date = WebDriverWait(driver, 2).until(
+            EC.visibility_of_element_located(
                 (By.XPATH, '//*[@id="container"]/div[2]/div[1]/div[1]/div[1]/span[2]')
             )
         )
-        content = WebDriverWait(driver, 4).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="editor"]'))
+        content = WebDriverWait(driver, 2).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="editor"]'))
         )
 
         array.append([project_name.text, j + 1, date.text, content.text])
@@ -62,7 +75,7 @@ for i in range(15, 27):
 
 with open("temp.txt", "w", encoding="utf8") as f:
     for x in range(len(array)):
-        f.write(f"{array[x][0]} - {array[x][1]}\n{array[x][3]}\n")
+        f.write(f"{array[x][0]} - {array[x][1]}\n{array[x][2]}\n{array[x][3]}\n")
 
 while True:
     pass
